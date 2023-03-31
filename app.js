563a459e67415867fd3189fa8939159ef7f01a4e
AmanPratapSinghChauhan <95226242+AmanPratapSinghChauhan@users.noreply.github.com>
2023-03-31 22:35:21 +0530

    Add files via upload
    Free typing website files uploaded

---
+++ app.js
@@ -0,0 +1,23 @@
+const express=require('express');
+const bodyparser=require('body-parser');
+const app=express();
+const ejs=require('ejs')
+var sp;
+var speedA=[];
+app.use(express.static("public"));
+app.use(express.json());
+app.use(bodyparser.urlencoded({extended:true}));
+app.set('view engine','ejs');
+app.get('/',function(req,res){
+  res.render('index');
+});
+app.post('/',function(req,res){
+  sp=req.body.speed;
+  speedA=req.body.speedArray;
+})
+app.get('/result',function(req,res){
+  res.render('result',{userSpeed:sp,speedAr:speedA});
+})
+app.listen(3000,function(){
+  console.log('server starts on port 3000');
+})
